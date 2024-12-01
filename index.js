a3142ead0dda68cca778af540c488c4a0684c6f6
Lemoncoke1009 <jhh5566727@yahoo.com>
2024-12-01 15:33:22 -0500

    a6

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import "dotenv/config";
 import session from "express-session";
 import cors from "cors";
 import express from 'express';
+import mongoose from "mongoose";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
 import CourseRoutes from "./Kanbas/Courses/routes.js";
@@ -9,6 +10,9 @@ import UserRoutes from "./Kanbas/Users/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
 import EnrollmentRoutes from "./Kanbas/Enrollments/routes.js";
+
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
+mongoose.connect(CONNECTION_STRING);
 
 const app = express();
 
