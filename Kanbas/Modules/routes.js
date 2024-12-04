d04414c25bd768390885573966dcf461651bc28a
Lemoncoke1009 <jhh5566727@yahoo.com>
2024-12-04 18:16:26 -0500

    a6

--- Kanbas/Modules/routes.js
+++ Kanbas/Modules/routes.js
@@ -1,15 +1,35 @@
 import * as modulesDao from "./dao.js";
 export default function ModuleRoutes(app) {
-  app.delete("/api/modules/:moduleId", (req, res) => {
+  app.delete("/api/modules/:moduleId", async (req, res) => {
     const { moduleId } = req.params;
-    modulesDao.deleteModule(moduleId);
+    await modulesDao.deleteModule(moduleId);
     res.sendStatus(204);
   });
-  app.put("/api/courses/:courseId", (req, res) => {
+
+  app.post("/api/courses/:courseId/modules", async (req, res) => {
     const { courseId } = req.params;
-    const courseUpdates = req.body;
-    const status = dao.updateCourse(courseId, courseUpdates);
+    const module = {
+      ...req.body,
+      course: courseId,
+    };
+    const newModule = await modulesDao.createModule(module);
+    res.send(newModule);
+  });
+
+  app.get("/api/courses/:courseId/modules", async (req, res) => {
+    const { courseId } = req.params;
+    const modules = await modulesDao.findModulesForCourse(courseId);
+    res.json(modules);
+  });
+
+  app.put("/api/modules/:moduleId", async (req, res) => {
+    const { moduleId } = req.params;
+    const moduleUpdates = req.body;
+    const status = await modulesDao.updateModule(moduleId, moduleUpdates);
     res.send(status);
   });
+ 
+ 
+ 
  
 }
